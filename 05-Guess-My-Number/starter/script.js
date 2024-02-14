c6f52c5f32fb7560e9834d898418787522e53354
Ameer Suhail <kameer41099@gmail.com>
2024-02-14 08:01:14 +0530

    code refactored

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,47 +1,52 @@
 'use strict';
 
-const randomNumber = Math.floor(Math.random()* 20);
-console.log(randomNumber);
-// const randomNumber = 18;
+const randomNumber = Math.trunc(Math.random() * 20 + 1 );
 let highScore = 0
 let score = 20
+
+const displayMessage = (message) => {
+    document.querySelector(".message").textContent = message;
+}
+const displayScore = (score) => {
+    document.querySelector(".label-score").textContent = score;
+}
 
 document.querySelector('.check').addEventListener('click', () => {
     let input = document.querySelector('.guess').value;
 
     if(score === 1){
         score = 21;
-        document.querySelector(".message").textContent = "Start guessing"
-        document.querySelector(".label-score").textContent = ` 💯 Score: 0`
+        displayMessage("Start guessing")
+        displayScore(` 💯 Score: 0`)
         document.body.style.background = '#222' 
     }
     // let backGround = document.body.style.background
     if(input){
         if(Number(input) === randomNumber){
-            document.querySelector(".message").textContent = "🎆Hurray!!!! Guessed right🎆" 
+            displayMessage("🎆Hurray!! Guessed right🎆") 
             if(highScore<score){
                 highScore = score;
             }
             document.querySelector(".label-highscore").textContent = `🥇 Highscore : ${highScore}`
-            document.querySelector(".label-score").textContent = `🥇 Score : ${score}`
+            displayScore(`🥇 Score : ${score}`)
+            document.querySelector('.number').textContent = randomNumber;
             document.body.style.background = 'rgb(3, 241, 3)'
-        }else{
-            document.querySelector(".message").textContent = "❌Please try again !!!❌"
-            document.body.style.background = '#ee0808'
+        }else if(Number(input) !== randomNumber){
+             Number(input) < randomNumber ? displayMessage("❌Too Low !!!❌") : displayMessage("❌Too High !!!❌")
             score--;
-            document.querySelector(".label-score").textContent = `💯 Score: ${score}`
+            displayScore(`💯 Score: ${score}`)
         }
     }else{
-        document.querySelector(".message").textContent = "❌Please try again !!!❌"
-        document.body.style.background = '#ee0808'
+        displayMessage("❌Please enter a number !!!❌")
         score--;
-        document.querySelector(".label-score").textContent = `💯 Score: ${score}`
+        displayScore(`💯 Score: ${score}`)
     }
 });
 
 document.querySelector(".again").addEventListener("click", () => {
     score = 20;
-    document.querySelector(".message").textContent = "Start guessing"
-    document.querySelector(".label-score").textContent = ` 💯 Score: 0`
+    displayMessage("Start guessing")
+    displayScore(` 💯 Score: 0`)
+    document.querySelector('.number').textContent = '?';
     document.body.style.background = '#222'
 })
